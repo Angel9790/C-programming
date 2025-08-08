39e375e11b93b5f4b820c3a9b0794405cf08f51a
Angel9790 <angelbavarva@gmail.com>
2025-08-08 15:57:20 +0530

    Add files via upload

---
+++ practical 14/practical14.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int choice;
+    float withdraw_amount,total_amount=5000;
+x:
+    printf("Enter 1 for withdraw amount.\n");
+    printf("Enter 0 for exit.\n");
+    scanf("%d",&choice);
+    while(1)
+        switch(choice)
+        {
+        case 1:
+            printf("Enter your withdraw amount:\n");
+            scanf("%f",&withdraw_amount);
+            if (withdraw_amount<=total_amount)
+            {
+
+                total_amount=total_amount-withdraw_amount;
+                printf("your total amount is:%f\n",total_amount);
+                goto x;
+                break;
+            }
+            else
+            {
+                printf("your balance in insufficient.\n");
+            }
+        case 0:
+            return 1;
+        }
+
+}
